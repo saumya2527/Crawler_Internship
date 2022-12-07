b0c4916ccf7736cebfcb01184b716e754c6379fb
ayushigupta931 <ayushigupta931@gmail.com>
2022-12-07 13:46:29 +0530

    time tracking

--- flipkart_crawler.mjs
+++ flipkart_crawler.mjs
@@ -1,10 +1,15 @@
 import { PlaywrightCrawler} from 'crawlee';
 
 const crawler = new PlaywrightCrawler({
+    maxRequestsPerCrawl: 5,
     requestHandler: async ({ page, request, enqueueLinks }) => {
+        
+        const start = Date.now();
+        
         console.log(`Processing: ${request.url}`)
+
         if (request.label === 'DETAIL') {
-           
+        
         } else {
             // This means we're either on the start page, with no label,
             // or on a list page, with LIST label.
@@ -17,12 +22,19 @@ const crawler = new PlaywrightCrawler({
 
             // In addition to adding the listing URLs, we now also
             // add the detail URLs from all the listing pages.
-            // await page.waitForSelector('._2UzuFa');
-            // await enqueueLinks({
-            //     selector: '._2UzuFa',
-            //     label: 'DETAIL', // <= note the different label
-            // })
+            await page.waitForSelector('._2UzuFa');
+            await enqueueLinks({
+                selector: '._2UzuFa',
+                label: 'DETAIL', // <= note the different label
+            })
         }
+
+        const end = Date.now();
+        console.log(`Request completed: ${request.url}`);
+        console.log(`Start time: ${start} ms`);
+        console.log(`End time: ${end} ms`);
+        console.log(`Execution time: ${end - start} ms`);
+
     },
 });
 
